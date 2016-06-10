cf90ff424104869cb70f326f4b8ba66eec3a5b75
Sukhmanbir Kaur <sukhmanbir.kaur@outlook.com>
2016-06-10 14:59:32 -0400

    Added create and delete functionality to DepartmentDetails.aspx

--- COMP2007-Lab03/DepartmentDetails.aspx
+++ COMP2007-Lab03/DepartmentDetails.aspx
@@ -1,5 +1,28 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="DepartmentDetails.aspx.cs" Inherits="COMP2007_Lab03.DepartmentDetails" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-</asp:Content>
+﻿<%@ Page Title="Department Details" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="DepartmentDetails.aspx.cs" Inherits="COMP2007_Lab03.DepartmentDetails" %>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="conatiner">
+        <div class="row">
+            <div class="col-md-offset-3 col-md-6">
+                <h1>Department Details</h1>
+                <h5>All Fields are required</h5>
+                <br />
+                <div class="form-group">
+
+                    <label class="control-label" for="DepartmentNameTextBox">Department Name</label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="DepartmentNameTextBox"
+                        placeholder="Department Name" required="true" MaxLength="100"></asp:TextBox>
+
+                    <label class="control-label" for="BudgetTextBox">Budget</label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="BudgetTextBox"
+                        placeholder="Budget" required="true" TextMode="Number"></asp:TextBox>
+
+                </div>
+                <div class="text-right">
+                    <asp:Button Text="Cancel" ID="CancelButton" runat="server" CssClass="btn btn-warning" UseSubmitBehavior="false" CausesValidation="false" OnClick="CancelButton_Click" />
+                    <asp:Button Text="Save" ID="SaveButton" runat="server" CssClass="btn btn-primary" OnClick="SaveButton_Click" />
+                </div>
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- COMP2007-Lab03/DepartmentDetails.aspx.cs
+++ COMP2007-Lab03/DepartmentDetails.aspx.cs
@@ -5,13 +5,87 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//required for EF DB access
+using COMP2007_Lab03.Models;
+using System.Web.ModelBinding;
+
 namespace COMP2007_Lab03
 {
     public partial class DepartmentDetails : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if ((!IsPostBack) && (Request.QueryString.Count > 0))
+            {
+                this.GetDepartment();
+            }
+        }
 
+        protected void GetDepartment()
+        {
+            //populate the form with existing department data from the db
+            int DepartmentID = Convert.ToInt32(Request.QueryString["DepartmentID"]);
+
+            //connect to the EF DB
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                //populate a department instance with the DepartmentID from the URL parameter
+                Department updatedDepartment = (from department in db.Departments
+                                          where department.DepartmentID == DepartmentID
+                                                select department).FirstOrDefault();
+
+
+                //map the department properties to the form controls
+                if (updatedDepartment != null)
+                {
+                    DepartmentNameTextBox.Text = updatedDepartment.Name;
+                    BudgetTextBox.Text = Convert.ToString(updatedDepartment.Budget);
+                }
+            }
+        }
+
+        protected void CancelButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~.Departments.aspx");
+        }
+
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            //connect to the EF DB
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                //use department model to save the object
+                Department newDepartment = new Department();
+
+                int DepartmentID = 0;
+                if (Request.QueryString.Count > 0)
+                {
+                    //get the id from the URL
+                    DepartmentID = Convert.ToInt32(Request.QueryString["DepartmentID"]);
+
+                    //get the current department fromt the EF DB
+                    newDepartment = (from department in db.Departments
+                                  where department.DepartmentID == DepartmentID
+                                  select department).FirstOrDefault();
+                }
+
+                newDepartment.Name = DepartmentNameTextBox.Text;
+                newDepartment.Budget = Convert.ToInt32(BudgetTextBox.Text);
+
+                //adds new department to the Department Table collection
+
+                //check to see if new department is being added
+                if (DepartmentID == 0)
+                {
+                    db.Departments.Add(newDepartment);
+                }
+
+                //save changes - run an update
+                db.SaveChanges();
+
+                //redirect to the updated department table
+                Response.Redirect("~/Departments.aspx");
+            }
         }
     }
 }
